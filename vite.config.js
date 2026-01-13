c75c984b0c52c9b7d9bfef7b85c14f90787642df
Alisa Kapranchikova <kapranchikovaa@duerkopp-adler.com>
2026-01-13 12:24:35 +0100

    изменены стили

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,17 @@
-import { defineConfig } from 'vite'
-import basicSsl from '@vitejs/plugin-basic-ssl';
-import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { readFileSync } from 'node:fs'
+import { fileURLToPath } from 'node:url'
+import { join } from 'node:path'
 
-// https://vite.dev/config/
+import { defineConfig } from 'vite'
+
+const __dirname = fileURLToPath(new URL('.', import.meta.url))
+
 export default defineConfig({
-  plugins: [ 
-    basicSsl(),
-    svelte({
-      compilerOptions: {
-        compatibility: {
-          componentApi: 4
-        }
-      }
-    })],
   server: {
-    https: true // Enables the HTTPS server
-  }
+    https: {
+      key: readFileSync(join(__dirname, 'certs', 'localhost-key.pem')),
+      cert: readFileSync(join(__dirname, 'certs', 'localhost-cert.pem')),
+    },
+    open: true,
+  },
 })
